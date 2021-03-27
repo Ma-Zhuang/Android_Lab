e6519c7be931da8e9543ed466cdcc7ed12cb7b56
Ma-Zhuang <2686224016@qq.com>
2021-03-27 22:31:06 +0000

    Labsheet7_solutions

--- Week7/Bundles_and_Threads/app/src/androidTest/java/uk/ac/bham/student/week7_bundles_and_threads/ExampleInstrumentedTest.java
+++ Week7/Bundles_and_Threads/app/src/androidTest/java/uk/ac/bham/student/week7_bundles_and_threads/ExampleInstrumentedTest.java
@@ -1,13 +1,28 @@
 package uk.ac.bham.student.week7_bundles_and_threads;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +37,78 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("uk.ac.bham.student.week7_bundles_and_threads", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityScenarioRule act = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void checkViews(){
+        onView(withId(R.id.start)).check(matches(isDisplayed()));
+        onView(withId(R.id.stop)).check(matches(isDisplayed()));
+        onView(withId(R.id.stop)).check(matches(isDisplayed()));
+        onView(withId(R.id.timer)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void startStopResetTimer(){
+        onView(withId(R.id.start)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {}
+
+        onView(withId(R.id.stop)).perform(click());
+        try {
+            //Give the clock time to settle
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {}
+
+        String timeNow = getText(withId(R.id.timer));
+        onView(withId(R.id.timer)).check(matches(not(withText("00:00:00"))));
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {}
+
+        onView(withId(R.id.timer)).check(matches(withText(timeNow)));
+        onView(withId(R.id.start)).perform(click());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {}
+
+        onView(withId(R.id.timer)).check(matches(not(withText(timeNow))));
+
+        onView(withId(R.id.reset)).perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {}
+
+        onView(withId(R.id.timer)).check(matches(withText("00:00:00")));
+    }
+
+    // Thanks StackOverflow for this!
+    // https://stackoverflow.com/questions/23381459/how-to-get-text-from-textview-using-espresso
+    String getText(final Matcher<View> matcher) {
+        final String[] stringHolder = { null };
+        onView(matcher).perform(new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(TextView.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "getting text from a TextView";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                TextView tv = (TextView)view; //Save, because of check in getConstraints()
+                stringHolder[0] = tv.getText().toString();
+            }
+        });
+        return stringHolder[0];
     }
 }
--- Week7/Bundles_and_Threads/app/src/main/java/uk/ac/bham/student/week7_bundles_and_threads/MainActivity.java
+++ Week7/Bundles_and_Threads/app/src/main/java/uk/ac/bham/student/week7_bundles_and_threads/MainActivity.java
@@ -3,12 +3,60 @@ package uk.ac.bham.student.week7_bundles_and_threads;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState!=null){
+            seconds =savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+        runTimer();
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+    }
+
+    private void runTimer() {
+        final TextView timer = (TextView) findViewById(R.id.timer);
+        final Handler thread = new Handler();
+        thread.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/360;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%02d:%02d:%02d",hours, minutes, seconds);
+                timer.setText(time);
+                if(running){
+                    seconds += 1;
+                }
+                thread.postDelayed(this,1000);
+            }
+        });
+    }
+    public void timerStart(View view){
+        running = true;
+    }
+
+    public void timerStop(View view){
+        running = false;
+    }
+
+    public void timerReset(View view){
+        running = false;
+        seconds = 0;
     }
 }

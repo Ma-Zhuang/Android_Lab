cce3935ff7b6594427dc9b7db3fe2488f8ea5301
Ma-Zhuang <2686224016@qq.com>
2021-03-27 22:15:18 +0000

    Labsheet7

---
+++ Week7/Bundles_and_Threads/app/src/main/java/uk/ac/bham/student/week7_bundles_and_threads/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.bham.student.week7_bundles_and_threads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

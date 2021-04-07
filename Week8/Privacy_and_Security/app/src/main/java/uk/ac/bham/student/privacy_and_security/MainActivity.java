c42193635796f9cfac2780ecab6a378740bd0c10
Ma-Zhuang <2686224016@qq.com>
2021-04-07 17:17:05 +0100

    Week8 solution

---
+++ Week8/Privacy_and_Security/app/src/main/java/uk/ac/bham/student/privacy_and_security/MainActivity.java
@@ -0,0 +1,52 @@
+package uk.ac.bham.student.privacy_and_security;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPref;
+    FragmentManager fm = getSupportFragmentManager();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        ((EditText)findViewById(R.id.borrowerID)).setText(sharedPref.getString("borrower_id","No ID set."));
+    }
+
+    public void setBorrowerIDClick(View view) {
+        EditText bID = (EditText) findViewById(R.id.borrowerID);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("borrower_id", bID.getText().toString());
+        editor.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.options:
+                Intent i = new Intent(MainActivity.this, OptionsActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ Week8/Privacy_and_Security/app/src/main/java/uk/ac/bham/student/privacy_and_security/OptionsActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.bham.student.privacy_and_security;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
